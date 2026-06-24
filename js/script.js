c4b8f494cf01cb31cf3811527f4d7436257aed2a
CosminDrilea <117672814+CosminDrilea@users.noreply.github.com>
2026-06-24 11:52:35 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -125,3 +125,23 @@ lightbox.style.display = "none";
 }
 
 });
+/* =========================
+   NAVBAR SCROLL
+========================= */
+
+window.addEventListener("scroll", () => {
+
+const navbar =
+document.querySelector(".navbar");
+
+if(window.scrollY > 50){
+
+navbar.classList.add("scrolled");
+
+}else{
+
+navbar.classList.remove("scrolled");
+
+}
+
+});
